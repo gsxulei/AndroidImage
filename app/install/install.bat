9f21fde0cdb8890b25c362fd2159fd3aaf2b68c1
徐雷 <31539634+gsxulei@users.noreply.github.com>
2018-07-13 22:23:55 +0800

    添加安装启动bat脚本

---
+++ app/install/install.bat
@@ -0,0 +1,16 @@
+set buildType=%1
+set apkName=%2
+
+::set projectPath=.\build\outputs\apk\debug\
+::set apkName=%projectPath%app-debug.apk
+
+echo buildType=%buildType%
+
+echo apkName=%apkName%
+
+::adb install -r %apkName%
+
+::if "%buildType%"=="debug" (
+::adb install -r ..\build\outputs\apk\debug\app-debug.apk
+::adb shell am start -a -n com.x62.simple/.MainActivity
+::) else (echo "release type do not install")
