662a2b348f9d4234d0c085d055ecb50dd2a8f1e1
徐雷 <31539634+gsxulei@users.noreply.github.com>
2019-04-19 17:30:43 +0800

    多渠道按照appId运行启动，生成apk文件名加时间戳

--- app/install/install.bat
+++ app/install/install.bat
@@ -1,17 +1,18 @@
 set buildType=%1
 set apkName=%2
+set applicationId=%3
 
 ::set projectPath=.\build\outputs\apk\debug\
 ::set apkName=%projectPath%app-debug.apk
 
 echo buildType=%buildType%
-
 echo apkName=%apkName%
+echo applicationId=%applicationId%
 
 ::adb install -r %apkName%
 
 if "%buildType%"=="debug" (
 ::adb install -r ..\build\outputs\apk\debug\app-debug.apk
 adb install -r %apkName%
-adb shell am start -a -n com.x62.gsxulei.demo/app.MainActivity
+adb shell am start -a -n %applicationId%/app.MainActivity
 ) else (echo "release type do not install")
